1be878904dd77d1c63fcc388ce9fc716a778743c
RubMaker <12312030@mail.sustech.edu.cn>
2025-04-23 17:30:09 +0800

    init

---
+++ CS202_project.srcs/sources_1/new/test_init.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/23/2025 05:29:34 PM
+// Design Name: 
+// Module Name: test_init
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test_init(
+
+    );
+endmodule
